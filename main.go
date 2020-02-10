93406f6dba3e5751552901bb1018bf7f728927f6
Richard Lander <landerr@vmware.com>
2020-02-10 11:29:00 -0500

    Revert to simplified logging
    Signed-off-by: Richard Lander <landerr@vmware.com>

--- main.go
+++ main.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 )
-
-var (
-	Info *log.Logger
-)
-
-func initLog(infoHandle io.Writer) {
-	Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-}
 
 func main() {
 
-	initLog(os.Stdout)
-
-	Info.Println("Building Apps For K8s app starting")
+	log.Println("Starting: Building Apps For K8s app")
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8000", nil)
@@ -28,6 +16,6 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	Info.Printf("Request received from %s", r.RemoteAddr)
+	log.Printf("Request received from %s", r.RemoteAddr)
 	fmt.Fprintf(w, "Building Apps For K8s app says Hi")
 }
