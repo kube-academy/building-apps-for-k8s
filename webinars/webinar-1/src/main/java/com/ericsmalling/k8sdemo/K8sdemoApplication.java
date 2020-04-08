deb75c03cab7f3a1fe4c9e879ae99c3b2327499f
smallinge <smallinge@localhost.com>
2020-04-08 11:29:46 -0500

    Initial spring-boot app for webinar 1

---
+++ webinars/webinar-1/src/main/java/com/ericsmalling/k8sdemo/K8sdemoApplication.java
@@ -0,0 +1,30 @@
+package com.ericsmalling.k8sdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class K8sdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+
+}
---
+++ webinars/webinar-1/src/test/java/com/ericsmalling/k8sdemo/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.ericsmalling.k8sdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest
+{
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        String expected = "Hello from Kube Academy, "+ System.getProperty("user.name")+" !\n";
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expected)));
+    }
+}
